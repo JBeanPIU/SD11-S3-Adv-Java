4b026383729ac61b5eb9ad8014774130229cb943
Cameron Beanland <cameronbeanland@hotmail.com>
2024-10-31 16:29:44 -0230

    Update Library.java
    Added all features (I think)

--- MidtermSprint/Library.java
+++ MidtermSprint/Library.java
@@ -2,10 +2,86 @@
 // By: Cameron Beanland
 // Date: October 25th, 2024
 
-// Following needs to be done:
-// Properties: Libary items, authors, and patrons files. (do those first), and should have methods to search for library items by title, author, or ISBNM, and to 
-//             borrow or return said items.
+import java.util.*;
 
 public class Library {
-    
+    // collection storing the library items, authors, & patrons
+    private List<LibraryItem> libraryItems = new ArrayList<>();
+    private List<Author> authors = new ArrayList<>();
+    private List<Patron> patrons = new ArrayList<>();
+
+
+    /* -- ADDING METHODS -- */
+    public void addLibraryItem(LibraryItem item) { // add new items
+        libraryItems.add(item);
+    }
+
+    public void addAuthor(Author author) { // add new author
+        authors.add(author);
+    }
+
+    public void addPatron(Patron patron) { // add new patron
+        patrons.add(patron);
+    }
+
+    /* -- SEARCH METHODS -- */
+    public LibraryItem searchTitle(String title) { // can search for title 
+        for (LibraryItem item : libraryItems) {
+            if (item.getTitle().equalsIgnoreCase(title)) {
+                return item;
+            }
+        }
+        return null; // only if there's no match
+    }
+
+    public LibraryItem searchISBN(String ISBN) { // search by ISBN
+        for (LibraryItem item : libraryItems) {
+            if (item.getISBN().equals(ISBN)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public List<LibraryItem> searchAuthor(String authorName) { // search by author
+        List<LibraryItem> results = new ArrayList<>();
+        for (LibraryItem item : libraryItems) {
+            if (item.getAuthor().authorName().equalsIgnoreCase(authorName)) {
+                results.add(item);
+            }
+        }
+        return results;
+    }
+
+    /* -- BORROW/RETURN METHODS -- */
+    public boolean borrowItem(String title, Patron patron) {
+        LibraryItem item = searchTitle(title);
+        if (item != null && item.isAvailable()) {
+            if (item.borrowItem()) {
+                patron.borrowItem(title);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean returnItem(String title, Patron patron) {
+        if (patron.checkBorrowedItem(title)) {
+            LibraryItem item = searchTitle(title);
+            if (item != null) {
+                item.returnItem();
+                patron.returnItem(title);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void displayLibraryItems() { // display information for all items
+        for (LibraryItem item : libraryItems) {
+            System.out.println(item.libraryInfo());
+            System.out.println("Format: " + item.getFormat());
+        }
+    }
+
 }
