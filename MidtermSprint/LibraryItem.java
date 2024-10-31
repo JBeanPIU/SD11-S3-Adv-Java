05c2aff7237fc6936166c930309ead6810c889c8
Cameron Beanland <cameronbeanland@hotmail.com>
2024-10-31 02:23:49 -0230

    Update LibraryItem.java
    Added all properties, constructors, getters/setters & methods required. May edit at some point, but the point of this class is to allow editing of the library items in this sprint.

--- MidtermSprint/LibraryItem.java
+++ MidtermSprint/LibraryItem.java
@@ -1,15 +1,106 @@
 // Program: LibaryItem.java, used to allow editing of library items (whether to edit, add, or delete items from the library.)
 // By: Cameron Beanland
-// Date: October 25th, 2024
+// Date: October 31st, 2024
 
-// Requirements: A class heirarchy that represents library items, such as Periodicals and Books
-
-// Periodicals can be Printed or electronic. Books can be printed, electronic and audio.
-
-// System overall should allow librarians to add new library item, exit existing items, and delete items from library. 
-
-// Each item should have the following: ID, title, author, ISBN, publisher, & number of copies and other attributes specific to their type.
 
 public class LibraryItem {
+    // all basic props for library items
+    private String id;
+    private String title;
+    private String author;
+    private String ISBN;
+    private String publisher;
+    private Integer copyNum; // weird naming but used to identify the number of available copies
+
+
+    // constructors
+    public LibraryItem(String id, String title, String author, String ISBN, String publisher, Integer copyNum) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.publisher = publisher;
+        this.copyNum = copyNum;
+    }
+
+
+    /* -- GETTERS -- */
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public Integer getCopyNum() {
+        return copyNum;
+    }
     
+
+    /* -- SETTERS -- */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setCopyNum(Integer copyNum) { 
+        this.copyNum = copyNum;
+    }
+
+
+    /* -- METHODS -- */
+    public boolean borrowItem() { // this method is to borrow a copy of an item
+        if (copyNum > 0) {
+            copyNum--;
+            return true; // borrowed successfully
+        } else {
+            System.out.println("Sorry! This item is currently checked out.");
+            return false; // prints statement if there are no copies left
+        }
+    }
+
+    public void returnItem() { // method to return copy of item
+        copyNum++;
+    }
+
+    public String libraryInfo() { // displays all item information in this class
+        return "ID: " + id +
+               "\nTitle: " + title +
+               "\nAuthor: " + author +
+               "\nISBN: " + ISBN +
+               "\nPublisher: " + publisher +
+               "\nAvailable Copies: " + copyNum;
+    }
+
+    public boolean isAvailable() { // checks for availability
+        return copyNum > 0;
+    }
 }
