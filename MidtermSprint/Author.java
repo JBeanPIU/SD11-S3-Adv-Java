85734113cf50a4aeb4c107289224ba83997ffb90
Cameron Beanland <cameronbeanland@hotmail.com>
2024-10-31 00:19:22 -0230

    Update Author.java
    Added all features from initial commit (had a little note describing what features to add), mostly it lol

--- MidtermSprint/Author.java
+++ MidtermSprint/Author.java
@@ -2,9 +2,62 @@
 // By: Cameron Beanland
 // Date: October 25th, 2024
 
-// Following needs to be done:
-// Properties include: Name, DOB, and list of items they've written.
+// most implementations should be added, but any extra commits may still happen lol
+
+import java.util.*; // adds all classes in the java.util package
 
 public class Author {
-    
+    // properties
+    private String name;
+    private Date dateOfBirth;
+    private List<String> items; // a list of items
+
+    // constructors
+    public Author(String name, Date dateOfBirth, List<String> items) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.items = items;
+    }
+
+
+    /* -- GETTERS -- */
+    public String getName() {
+        return name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public List<String> getItems() {
+        return items;
+    }
+
+
+    /* -- SETTERS -- */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setItems(List<String> items) {
+        this.items = items;
+    }
+
+
+    /* -- METHODS -- */
+    public void addItem(String item) {
+        items.add(item);
+    }
+
+    public void removeItem(String item) {
+        items.remove(item);
+    }
+
+    public String toString() {
+        return "Author Name: " + name + " Date: " + dateOfBirth + " Written works: " + items;
+    }
 }
